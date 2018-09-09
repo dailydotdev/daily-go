42cf79ccab2ee375adca944fa7b5d84218bc95bf
Ido Shamun <ido@elegantmonkeys.com>
2018-09-09 18:24:45 +0300

    refactor(notSupported): update not supported page

--- pages/notSupported.vue
+++ pages/notSupported.vue
@@ -19,23 +19,10 @@
       </DaTerminal>
       <p class="subtext primary">
         Currently, Daily Now PWA supports only mobile devices.
-        If you want to stay up to date you can download the browser extension.
+        We are working hard to deliver a desktop version, in the meantime, you can download our browser extension.
+        Stay tuned!
       </p>
       <DaStores></DaStores>
-    </div>
-    <div class="insane" ref="insane">
-      <DaInsanePost v-for="item in insane" :key="item.id" :post-id="item.id" :link="item.url"
-                    :title="item.title" :source="item.publication.name"
-                    :logo="item.publication.image"></DaInsanePost>
-    </div>
-    <div class="cards" ref="cards">
-      <DaPost v-for="item in cards" :key="item.id" :post-id="item.id" :link="item.url"
-              :img="item.image" :title="item.title" :source="item.publication.name"
-              :logo="item.publication.image" :size="item.size" :placeholder="item.placeholder"></DaPost>
-    </div>
-    <DaSwitch icon="bookmark" :checked="true"></DaSwitch>
-    <div class="footer">
-      <img src="~/assets/ido.jpeg" alt="Profile image"/>
     </div>
   </section>
 </template>
@@ -43,23 +30,12 @@
 <script>
 import DaTerminal from '../components/DaTerminal';
 import DaStores from '../components/DaStores';
-import { fetchLatestPosts } from '../services/api';
-import DaInsanePost from '../components/DaInsanePost';
-import DaPost from '../components/DaPost';
-import DaSwitch from '../components/DaSwitch';
 import { trackPage } from '../services/analytics';
 
 export default {
-  components: { DaPost, DaInsanePost, DaStores, DaTerminal, DaSwitch },
+  components: { DaStores, DaTerminal },
 
   middleware: 'supported',
-
-  data() {
-    return {
-      insane: [],
-      cards: [],
-    };
-  },
 
   mounted() {
     // Workaround as nuxt currently calls mounted twice
@@ -67,27 +43,10 @@ export default {
       import('../icons/bookmark');
 
       trackPage('notSupported');
-
-      fetchLatestPosts(new Date(), 0)
-        .then((posts) => {
-          this.insane = posts.slice(0, posts.length * 2 / 3);
-          this.cards = posts.slice(posts.length * 2 / 3, posts.length);
-        });
-    });
-
-    this.$el.addEventListener('wheel', (event) => {
-      const delta = event.deltaY;
-      this.$refs.cards.scrollTop = Math.max(0, Math.min(this.$refs.cards.scrollTop - delta, this.$refs.cards.scrollHeight));
-      this.$refs.insane.scrollTop = Math.max(0, Math.min(this.$refs.insane.scrollTop + delta, this.$refs.insane.scrollHeight));
     });
   }
 };
 </script>
-
-<style>
-@import '../styles/insane.pcss';
-@import '../styles/card.pcss';
-</style>
 
 <style scoped>
 .container {
@@ -132,55 +91,6 @@ export default {
 
   & .progress {
     padding: 0;
-  }
-}
-
-.insane {
-  position: absolute;
-  right: 0;
-  top: 73%;
-  width: 330px;
-  height: 290px;
-  overflow: hidden;
-  background: var(--color-background-highlight);
-  border-radius: var(--size-space);
-}
-
-.cards {
-  position: absolute;
-  bottom: 80%;
-  left: 60px;
-  width: 330px;
-  height: 330px;
-  overflow: hidden;
-}
-
-.switch {
-  position: absolute;
-  bottom: 12%;
-  left: 50%;
-}
-
-.footer {
-  position: absolute;
-  display: flex;
-  left: 3%;
-  bottom: 5%;
-  width: 500px;
-  height: 120px;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-  padding: calc(var(--size-space) * 3);
-  border-radius: 16px 16px 0 0;
-  background: var(--color-background-highlight);
-  box-shadow: 0 -1px 0 0 var(--color-background), 0 -8px 16px 0 rgba(0, 0, 0, 0.2);
-
-  & img {
-    width: 48px;
-    height: 48px;
-    border-radius: 4px;
-    object-fit: cover;
   }
 }
 </style>
