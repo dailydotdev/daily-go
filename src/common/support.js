7d4f46883357d66a6c1d17dcb86aed224322f48d
Ido Shamun <ido@elegantmonkeys.com>
2018-10-01 18:47:44 +0300

    chore: disable support check (qa purposes)

--- src/common/support.js
+++ src/common/support.js
@@ -1,3 +1,4 @@
 export default function () {
-  return window.CSS && window.CSS.supports && window.CSS.supports('--fake-var', 0);
+  return true;
+  // return window.CSS && window.CSS.supports && window.CSS.supports('--fake-var', 0);
 }
